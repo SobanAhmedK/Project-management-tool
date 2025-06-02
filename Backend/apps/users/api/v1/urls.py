b611a065615d22621a9ec4c2cf5aba583257ffaa
SobanAhmedK <ahmedsoban648@gmail.com>
2025-06-03 03:21:27 +0500

    adding path for token verification and refresh

--- Backend/apps/users/api/v1/urls.py
+++ Backend/apps/users/api/v1/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
 from rest_framework.routers import DefaultRouter
 from apps.users import views
 
@@ -11,5 +17,7 @@ router.register(r'admin/users', views.UserAdminViewSet, basename='admin-users')
 urlpatterns = [
     path('auth/register/', views.UserRegistrationView.as_view(), name='register'),
     path('auth/login/', views.UserLoginView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
    
 ] + router.urls
--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -116,8 +116,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20
 }
 
 
